61bfd6887aaeeae6109c1b199bcdca98630125af
Ömer İslamoğlu <omer@trinsyca.com>
2025-07-06 17:51:13 +0300

    influncer stories design update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -227,26 +227,6 @@
                                             Instagram'da Görüntüle
                                         </a>
                                     </div>
-                                    <div class="swiper-slide">
-                                        <div class="swiper mySwiper">
-                                            <div class="swiper-wrapper">
-                                                <div class="swiper-slide">Slide 1</div>
-                                                <div class="swiper-slide">Slide 2</div>
-                                                <div class="swiper-slide">Slide 3</div>
-                                                <div class="swiper-slide">Slide 4</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="swiper mySwiper">
-                                            <div class="swiper-wrapper">
-                                                <div class="swiper-slide">Slide 1</div>
-                                                <div class="swiper-slide">Slide 2</div>
-                                                <div class="swiper-slide">Slide 3</div>
-                                                <div class="swiper-slide">Slide 4</div>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
