64779751e8088e2f608f5840c0c89e861134443f
Ömer İslamoğlu <omer@trinsyca.com>
2025-07-07 16:59:49 +0300

    fixed-contact url link updates

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="footer-container section-container">
+        <div class="footer-top">
+            <ul class="footer-menu-list">
+                <li class="footer-menu">
+                    <a href="/" class="footer-logo">
+                        <div class="logo">
+                            <h1><span data-text="Seval Sırakaya">Seval Sırakaya</span> <span data-text="Güzellik Salonu">Güzellik Salonu</span></h1>
+                        </div>
+                    </a>
+                </li>
+                <li class="footer-menu">
+                    <b><a class="footer-link" href="https://www.kolayrandevu.com/isletme/seval-sirakaya-guzellik-ve-sac?website=1"><span>Randevu Al</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M14.5 13.5v-7h-7m7 0l-8 8"/></svg></a></b>
+                    <a class="footer-link" href="#hizmetler"><span>Hizmetler</span></a>
+                    <a class="footer-link" href="#iletisim"><span>İletişim</span></a>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <div class="footer-copyright">
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#333" d="M8.75 10a3.25 3.25 0 1 1 6.5 0a3.25 3.25 0 0 1-6.5 0"/><path fill="#333" fill-rule="evenodd" d="M3.774 8.877a8.04 8.04 0 0 1 8.01-7.377h.432a8.04 8.04 0 0 1 8.01 7.377a8.7 8.7 0 0 1-1.933 6.217L13.5 20.956a1.937 1.937 0 0 1-3 0l-4.792-5.862a8.7 8.7 0 0 1-1.934-6.217M12 5.25a4.75 4.75 0 1 0 0 9.5a4.75 4.75 0 0 0 0-9.5" clip-rule="evenodd"/></svg><span><strong>Çobançeşme, Mihriban Sk. 49/51, 34196 Bahçelievler/İstanbul</strong></span></p>
+            </div>
+            <div class="footer-social">
+                <a target="_blank" href="https://wa.me/905413070305?text=Merhaba, randevu almak istiyorum." class="footer-link">WhatsApp</a>
+                <a target="_blank" href="https://instagram.com/sevalsirakayaguzellik" class="footer-link">Instagram</a>
+            </div>
+        </div>
+        <div class="footer-policy">
+            <a class="footer-link" href="/gizlilik-ve-kosullar">Gizlilik ve Kullanım Koşulları</a>
+        </div>
+        <div class="footer-signature">
+            <p class="signature">
+                Created by <a href="https://trinsyca.com" target="_blank" class="creator">TrinsyCa</a>
+            </p>
+        </div>
+    </div>
+</footer>
