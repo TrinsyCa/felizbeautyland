4dc874fe6b888f7544a057e337f923c1b6357f12
Ömer İslamoğlu <omer@trinsyca.com>
2025-07-07 18:04:06 +0300

    policies page

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,15 +1,15 @@
 <nav>
     <div class="nav-container">
         <div class="nav-logo">
-            <a href="{{ url('#') }}">
+            <a href="{{ url('/#') }}">
                 <img src="{{ asset('assets/images/logo/feliz-logo.webp') }}" alt="Feliz Beautyland Logo">
             </a>
         </div>
         <ul class="nav-links">
-            <li><a href="{{ url('#') }}">Anasayfa</a></li>
+            <li><a href="{{ url('/#') }}">Anasayfa</a></li>
             <li><a onclick="reservationSection();">Hizmetler</a></li>
-            <li><a href="{{ url('#galeri') }}">Galeri</a></li>
-            <li><a href="{{ url('#iletisim') }}">İletişim</a></li>
+            <li><a href="{{ url('/#galeri') }}">Galeri</a></li>
+            <li><a href="{{ url('/#iletisim') }}">İletişim</a></li>
         </ul>
         <div class="close-reservation">
             <button onclick="reservationSection();">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/gizlilik-ve-kullanim-kosullari', function () {
+    return view('pages.policies');
+});
