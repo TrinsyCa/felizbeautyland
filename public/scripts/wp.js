24c5045442af8c7148218210626f3074452829af
Ömer İslamoğlu <omer@trinsyca.com>
2025-07-16 01:16:38 +0300

    logo changed

--- public/scripts/wp.js
+++ public/scripts/wp.js
@@ -5,7 +5,7 @@ function formatDate(inputDate) {
     return formattedDate;
 }
 
-function wp() {
+function bookNow() {
     var name = document.getElementsByName("name")[0].value;
     var time = document.getElementsByName("time")[0].value;
     var datetime = document.getElementsByName("datetime")[0].value;
