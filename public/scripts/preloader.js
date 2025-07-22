4b22c0269533ac031f9cd9e24564dab3dadbc157
Ömer İslamoğlu <omer@trinsyca.com>
2025-07-22 20:23:33 +0300

    preloader font load update

--- public/scripts/preloader.js
+++ public/scripts/preloader.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   const preloader = document.querySelector('.preloader');
-  if (preloader) {
+  if (!preloader) return;
+
+  // Fontu yükle
+  const font = new FontFace(
+    'Quicksand',
+    'url(/fonts/quicksand/Quicksand-VariableFont_wght.ttf)'
+  );
+
+  font.load().then(function() {
+    document.fonts.add(font);
     preloader.classList.add('loaded');
     document.querySelector('html').removeAttribute('style');
-  }
+  }).catch(function(error) {
+    preloader.classList.add('loaded');
+    document.querySelector('html').removeAttribute('style');
+  });
 });
