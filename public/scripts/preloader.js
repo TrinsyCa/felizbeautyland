9f5d22368e01ac992b47920d059beeb0f4a5065e
Ömer İslamoğlu <omer@trinsyca.com>
2025-07-22 20:17:38 +0300

    preloader updates

--- public/scripts/preloader.js
+++ public/scripts/preloader.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const preloader = document.querySelector('.preloader');
   if (preloader) {
     preloader.classList.add('loaded');
+    document.querySelector('html').classList.remove('reservationActivated');
   }
 });
