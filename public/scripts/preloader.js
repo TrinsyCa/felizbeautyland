d0f3150f644f9d7203a279d83ec309528e970dbe
Ömer İslamoğlu <omer@trinsyca.com>
2025-07-22 20:15:02 +0300

    preloader fix

--- public/scripts/preloader.js
+++ public/scripts/preloader.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const preloader = document.querySelector('.loader');
+  const preloader = document.querySelector('.preloader');
   if (preloader) {
     preloader.classList.add('loaded');
   }
