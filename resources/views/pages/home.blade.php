0821400ddfd55724d93648d7d52620864ac4fa65
ommergur <farukomergur@gmail.com>
2025-10-25 12:25:11 +0300

    client's update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -611,7 +611,7 @@
                                                     </div>
                                                     <div class="reservation-box">
                                                         <p data-value="37-1">Cilt Bakımı</p>
-                                                        <span class="reservate-price">2.500 ₺</span>
+                                                        <span class="reservate-price">3.500 ₺</span>
                                                         <button class="reservate-btn">+ Randevu Ekle</button>
                                                     </div>
                                                     <div class="reservation-box">
